a1f206dbdc1d94859955dd3d35c5666c3365e927
Matthias Klumpp <matthias@tenstral.net>
2014-10-31 15:25:58 +0100

    Add LiInstaller skeleton

---
+++ src/li-installer.c
@@ -0,0 +1,85 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2014 Matthias Klumpp <matthias@tenstral.net>
+ *
+ * Licensed under the GNU Lesser General Public License Version 2.1
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the license, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * SECTION:li-installer
+ * @short_description: High level installation of IPK packages
+ */
+
+#include "config.h"
+#include "li-installer.h"
+
+typedef struct _LiInstallerPrivate	LiInstallerPrivate;
+struct _LiInstallerPrivate
+{
+
+};
+
+G_DEFINE_TYPE_WITH_PRIVATE (LiInstaller, li_installer, G_TYPE_OBJECT)
+
+#define GET_PRIVATE(o) (li_installer_get_instance_private (o))
+
+/**
+ * li_installer_finalize:
+ **/
+static void
+li_installer_finalize (GObject *object)
+{
+#if 0
+	LiInstaller *inst = LI_INSTALLER (object);
+	LiInstallerPrivate *priv = GET_PRIVATE (inst);
+#endif
+
+	G_OBJECT_CLASS (li_installer_parent_class)->finalize (object);
+}
+
+/**
+ * li_installer_init:
+ **/
+static void
+li_installer_init (LiInstaller *inst)
+{
+}
+
+/**
+ * li_installer_class_init:
+ **/
+static void
+li_installer_class_init (LiInstallerClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+	object_class->finalize = li_installer_finalize;
+}
+
+/**
+ * li_installer_new:
+ *
+ * Creates a new #LiInstaller.
+ *
+ * Returns: (transfer full): a #LiInstaller
+ *
+ **/
+LiInstaller *
+li_installer_new (void)
+{
+	LiInstaller *inst;
+	inst = g_object_new (LI_TYPE_INSTALLER, NULL);
+	return LI_INSTALLER (inst);
+}
---
+++ src/li-installer.h
@@ -0,0 +1,66 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2014 Matthias Klumpp <matthias@tenstral.net>
+ *
+ * Licensed under the GNU Lesser General Public License Version 2.1
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the license, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if !defined (__LIMBA_H) && !defined (LI_COMPILATION)
+#error "Only <limba.h> can be included directly."
+#endif
+
+#ifndef __LI_INSTALLER_H
+#define __LI_INSTALLER_H
+
+#include <glib-object.h>
+
+#define LI_TYPE_INSTALLER			(li_installer_get_type())
+#define LI_INSTALLER(obj)			(G_TYPE_CHECK_INSTANCE_CAST((obj), LI_TYPE_INSTALLER, LiInstaller))
+#define LI_INSTALLER_CLASS(cls)	(G_TYPE_CHECK_CLASS_CAST((cls), LI_TYPE_INSTALLER, LiInstallerClass))
+#define LI_IS_INSTALLER(obj)		(G_TYPE_CHECK_INSTANCE_TYPE((obj), LI_TYPE_INSTALLER))
+#define LI_IS_INSTALLER_CLASS(cls)	(G_TYPE_CHECK_CLASS_TYPE((cls), LI_TYPE_INSTALLER))
+#define LI_INSTALLER_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS((obj), LI_TYPE_INSTALLER, LiInstallerClass))
+
+G_BEGIN_DECLS
+
+typedef struct _LiInstaller		LiInstaller;
+typedef struct _LiInstallerClass	LiInstallerClass;
+
+struct _LiInstaller
+{
+	GObject			parent;
+};
+
+struct _LiInstallerClass
+{
+	GObjectClass		parent_class;
+	/*< private >*/
+	void (*_as_reserved1)	(void);
+	void (*_as_reserved2)	(void);
+	void (*_as_reserved3)	(void);
+	void (*_as_reserved4)	(void);
+	void (*_as_reserved5)	(void);
+	void (*_as_reserved6)	(void);
+	void (*_as_reserved7)	(void);
+	void (*_as_reserved8)	(void);
+};
+
+GType			li_installer_get_type	(void);
+LiInstaller		*li_installer_new		(void);
+
+G_END_DECLS
+
+#endif /* __LI_INSTALLER_H */
